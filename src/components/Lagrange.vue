70622bd23de2d7fe4ca9cda0712af351cb30f0fb
Matheus Muriel <matheus.muriel@outlook.com>
2019-11-05 10:17:21 -0300

    Adicionado linhas F e X e os placeholders

--- src/components/Lagrange.vue
+++ src/components/Lagrange.vue
@@ -3,23 +3,48 @@
     <img src="https://i.imgur.com/vBqV9Gx.jpg" class="img-principal">
 
     <b-container class="inputs">
-      <b-row>
+      <b-row class="linha-input" align-h="center">
         <b-col>
-          <b-input v-model="valor1"></b-input>
+          <b-input class="input-input" v-model="valorX1" placeholder="X1"/>
         </b-col>
         <b-col>
-          <b-input v-model="valor2"></b-input>
+          <b-input class="input-input" v-model="valorX2" placeholder="X2"/>
         </b-col>
         <b-col>
-          <b-input v-model="valor3"></b-input>
+          <b-input class="input-input" v-model="valorX3" placeholder="X3"/>
         </b-col>
         <b-col>
-          <b-input v-model="valor4"></b-input>
+          <b-input class="input-input" v-model="valorX4" placeholder="X4"/>
         </b-col>
         <b-col>
-          <b-input v-model="valor5"></b-input>
+          <b-input class="input-input" v-model="valorX5" placeholder="X5"/>
         </b-col>
       </b-row>
+
+      <b-row class="linha-input" align-h="center">
+        <b-col>
+          <b-input class="input-input" v-model="valorF1" placeholder="F1"/>
+        </b-col>
+        <b-col>
+          <b-input class="input-input" v-model="valorF2" placeholder="F2"/>
+        </b-col>
+        <b-col>
+          <b-input class="input-input" v-model="valorF3" placeholder="F3"/>
+        </b-col>
+        <b-col>
+          <b-input class="input-input" v-model="valorF4" placeholder="F4"/>
+        </b-col>
+        <b-col>
+          <b-input class="input-input" v-model="valorF5" placeholder="F5"/>
+        </b-col>
+      </b-row>
+
+      <b-row class="linha-input" align-h="center">
+        <b-col cols="2">
+          <b-input class="input-input" v-model="valorX" placeholder="X"/>
+        </b-col>
+      </b-row>
+
       <b-row class="justify-content-md-center btn-input">
         <b-button variant="outline-primary" @click="clickCalcular">Calcular</b-button>
       </b-row>
@@ -28,23 +53,29 @@
 </template>
 
 <script>
-import axios from 'axios'
+// import axios from 'axios'
 
 export default {
   data: function () {
     return {
-      valor1: 0,
-      valor2: 0,
-      valor3: 0,
-      valor4: 0,
-      valor5: 0
+      valorX1: null,
+      valorX2: null,
+      valorX3: null,
+      valorX4: null,
+      valorX5: null,
+      valorF1: null,
+      valorF2: null,
+      valorF3: null,
+      valorF4: null,
+      valorF5: null,
+      valorX: null
     }
   },
   methods: {
     clickCalcular () {
       // axios
-        // .get('https://api.coindesk.com/v1/bpi/currentprice.json')
-        // .then(response => (console.log(response.data.bpi)))
+      // .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      // .then(response => (console.log(response.data.bpi)))
     }
   }
 }
@@ -68,5 +99,11 @@ export default {
 }
 .btn-input {
   margin: 30px;
+}
+.linha-input {
+  margin: 30px;
+}
+.linha-input input {
+  text-align: center;
 }
 </style>
